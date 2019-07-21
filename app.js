744a426e8d21f4234ba43b919d771a970e358617
Tyler Larrabee <tyer.k.larrabee@gmail.com>
2019-07-21 12:47:57 -0700

    Functional Scrape loading to dom

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // view engine setup
 const exphbs = require("express-handlebars");
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', scrapeRouter);
+app.use('/scrape', scrapeRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  db.Article.find({}).then((dbArticle) => {
+    res.render('index', { title: 'Express', article: dbArticle })
+  }).catch( err => {
+    res.render('error', {error: err})
+  })
 });
 
 module.exports = router;
