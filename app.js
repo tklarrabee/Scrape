42be49a42901a5e276ffc615cc3edc9405309007
Tyler Larrabee <tyer.k.larrabee@gmail.com>
2019-07-21 00:23:33 -0700

    models docs, nav bar

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const scrapeRouter = require('./routes/scrape');
+
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 
 // view engine setup
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 // app.set('views', path.join(__dirname, 'views'));
 
 
@@ -24,15 +37,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', scrapeRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -41,5 +54,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("App running on port " + PORT + "!");
+});
+
 
 module.exports = app;
