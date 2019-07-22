7565da065a179a429fd3859d41ab83a604aa4bd5
Tyler Larrabee <tyer.k.larrabee@gmail.com>
2019-07-21 23:37:16 -0700

    less than good

--- routes/scrape.js
+++ routes/scrape.js
@@ -9,39 +9,28 @@ var router = express.Router();
 //   res.send('respond with a resource');
 // });
 
-router.get("/", function(req, res) {
+router.get("/", (req, res) => {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://old.reddit.com/r/learnprogramming/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
+    const $ = cheerio.load(response.data);
+    const result ={}
+    $("p.title").each( (i, element) => {
+      result.title = $(element).children("a").text()
+      result.link = $(element).children("a").attr("href")
+      // console.log(result)
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-    });
-
+      .then( dbArticle => console.log(dbArticle))
+      .catch( err => console.log(err))
+    })
     // Send a message to the client
     res.send("Scrape Complete");
   });
 });
+
+router.post("/", (req, res) => {
+  db.Article.drop()
+})
+
 module.exports = router;
